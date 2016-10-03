2a6c4ce160bbea5c60be011aa4a004ec71cc497a
Rohan Kumar <rohan.carmel@gmail.com>
2016-10-04 00:25:19 +0530

    time difference calculator

--- app/src/main/java/com/dark/confess/Constants.java
+++ app/src/main/java/com/dark/confess/Constants.java
@@ -1,13 +1,14 @@
 package com.dark.confess;
 
-import android.*;
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by darshan on 29/09/16.
@@ -30,9 +31,47 @@ public class Constants {
     }
 
     public static String getCurrentTime() {
+        return (DateFormat.format("dd-MM-yyyy hh:mm:ss aa", new java.util.Date()).toString());
+    }
 
-        //TODO get the current time stamp in proper displayable format
-        return "12 am";
+    public static String timeDiff(String postTime) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss aa");
+        Date past = null;
+        try {
+            past = sdf.parse(postTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date now = new Date();
+
+        long duration = now.getTime() - past.getTime();
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(duration);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(duration);
+        long hours = TimeUnit.MILLISECONDS.toHours(duration);
+        long days = TimeUnit.MILLISECONDS.toDays(duration);
+
+        if (seconds < 60)
+            return seconds + " Seconds ago";
+        else {
+            if (minutes < 60)
+                return minutes + " Minutes ago";
+            else {
+                if (hours < 24)
+                    return hours + " Hours ago";
+                else {
+                    if (days < 30)
+                        return days + " Days ago";
+                    else {
+                        if ((days / 30) < 12)
+                            return (days / 30) + " Months ago";
+                        else
+                            return ((days / 30) / 12) + " Years ago";
+                    }
+                }
+            }
+        }
+
     }
 
 }
